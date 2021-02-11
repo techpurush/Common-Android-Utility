00919f30ed0eea600b8c5c6b22f491dd2b2c0bf5
Techpurush <techpurush.2020@gmail.com>
2021-02-11 20:46:50 +0530

    DialogUtils added.

---
+++ CommonAndroidUtility/src/main/java/com/techpurush/commonandroidutility/DialogUtils.java
@@ -0,0 +1,16 @@
+package com.techpurush.commonandroidutility;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
+public class DialogUtils {
+
+    public static void alert(@Nullable Context context,String msg){
+
+        new AlertDialog.Builder(context).setMessage(msg).show();
+    }
+
+    
+}
